a9937d40cd32874db3d614d439f010291c94c03a
ShenHaoWen <805154269@qq.com>
2018-01-09 17:54:13 +0800

    Initial Commit

---
+++ KTTextEditView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KTTextEditView
+//
+//  Created by Karthus on 2018/1/9.
+//  Copyright © 2018年 karthus. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ KTTextEditView/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  KTTextEditView
+//
+//  Created by Karthus on 2018/1/9.
+//  Copyright © 2018年 karthus. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
